e4a219d4bcd64764caa1024ecdc2071d2a6ab21b
significance <dan@1up.digital>
2018-11-09 10:21:53 +0000

    qf

--- swarm/storage/filestore.go
+++ swarm/storage/filestore.go
@@ -34,9 +34,9 @@ implementation for storage or retrieval.
 */
 
 const (
-	defaultLDBCapacity                = 5000000 // capacity for LevelDB, by default 5*10^6*4096 bytes == 20GB
+	defaultLDBCapacity                = 500000 // capacity for LevelDB, by default 5*10^6*4096 bytes == 20GB
 	defaultCacheCapacity              = 10000   // capacity for in-memory chunks' cache
-	defaultChunkRequestsCacheCapacity = 5000000 // capacity for container holding outgoing requests for chunks. should be set to LevelDB capacity
+	defaultChunkRequestsCacheCapacity = 500000 // capacity for container holding outgoing requests for chunks. should be set to LevelDB capacity
 )
 
 type FileStore struct {
